6e67f63d0066e6a5e63d8b96481bf9fd16550614
Yassinesahyane89 <yassine.sahyane89@gmail.com>
2022-10-20 09:04:20 +0100

    sprint2

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -13,22 +13,26 @@ let iconDone = "fa-regular fa-circle-check fa-2xl";
 let NbrTodo = document.getElementById("to-do-tasks-count");
 let NbrInprogress = document.getElementById("in-progress-tasks-count");
 let NbrDone = document.getElementById("done-tasks-count");
+let count = 1;
+let NomberDone = 1;
+let NomberTodo = 1;
+let NomberInprogress = 1;
 
-function button(count, icon, title, date, description, priority, type) {
+function button(count, icon, i) {
   let btn = `
         <button class="d-flex align-items-center border-0 border-top">
             <div class="col-1 text-success">
                 <i class="${icon}"></i> 
             </div>
             <div class="col-11 text-start">
-                <div class="fw-800">${title}</div>
+                <div class="fw-800">${tasks[i].title}</div>
                     <div class="">
-                        <div class="fw-100">#${count} created in ${date}</div>
-                        <div class="title1" title="${description}">${description}</div>
+                        <div class="fw-100">#${count} created in ${tasks[i].date}</div>
+                        <div class="title1" title="${tasks[i].description}">${tasks[i].description}</div>
                     </div>
                     <div class="">
-                        <span class="btn btn-primary btn-sm">${priority}</span>
-                        <span class="btn btn-secondary btn-sm">${type}</span>
+                        <span class="btn btn-primary btn-sm">${tasks[i].priority}</span>
+                        <span class="btn btn-secondary btn-sm">${tasks[i].type}</span>
                     </div>
             </div>
         </button>
@@ -36,55 +40,31 @@ function button(count, icon, title, date, description, priority, type) {
   return btn;
 }
 
+function add(i) {
+  if (tasks[i].status == "To Do") {
+    NbrTodo.innerHTML = NomberTodo;
+    ToDO.innerHTML += button(count, iconTOdo, i);
+    count++;
+    NomberTodo++;
+  }
+  if (tasks[i].status == "Done") {
+    NbrDone.innerHTML = NomberDone;
+    Done.innerHTML += button(count, iconDone, i);
+    count++;
+    NomberDone++;
+  }
+  if (tasks[i].status == "In Progress") {
+    NbrInprogress.innerHTML = NomberInprogress;
+    InProgress.innerHTML += button(count, iconInprogress, i);
+    count++;
+    NomberInprogress++;
+  }
+}
+
 function createTask() {
   // initialiser task form
-  let count = 1;
-  let NomberDone = 1;
-  let NomberTodo = 1;
-  let NomberInprogress = 1;
   for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].status == "To Do") {
-      NbrTodo.innerHTML = NomberTodo;
-      ToDO.innerHTML += button(
-        count,
-        iconTOdo,
-        tasks[i].title,
-        tasks[i].date,
-        tasks[i].description,
-        tasks[i].priority,
-        tasks[i].type
-      );
-      count++;
-      NomberTodo++;
-    }
-    if (tasks[i].status == "Done") {
-      NbrDone.innerHTML = NomberDone;
-      Done.innerHTML += button(
-        count,
-        iconDone,
-        tasks[i].title,
-        tasks[i].date,
-        tasks[i].description,
-        tasks[i].priority,
-        tasks[i].type
-      );
-      count++;
-      NomberDone++;
-    }
-    if (tasks[i].status == "In Progress") {
-      NbrInprogress.innerHTML = NomberInprogress;
-      InProgress.innerHTML += button(
-        count,
-        iconInprogress,
-        tasks[i].title,
-        tasks[i].date,
-        tasks[i].description,
-        tasks[i].priority,
-        tasks[i].type
-      );
-      count++;
-      NomberInprogress++;
-    }
+    add(i);
   }
   // Afficher le boutton save
 
@@ -93,12 +73,27 @@ function createTask() {
 
 createTask();
 function saveTask() {
+  document.getElementById("BtnSave").addEventListener("click", displayDate);
+
+  function displayDate() {
+    let Newdata = {
+      title: document.getElementById("recipient-name").value,
+      type: document.querySelector('input[name="flexRadioDefault"]:checked')
+        .value,
+      priority: document.getElementById("Proiority").value,
+      status: document.getElementById("status").value,
+      date: document.getElementById("date-input").value,
+      description: document.getElementById("message-text").value,
+    };
+    tasks.push(Newdata);
+    add(tasks.length - 1);
+  }
   // Recuperer task attributes a partir les champs input
   // Créez task object
   // Ajoutez object au Array
   // refresh tasks
 }
-
+saveTask();
 function editTask(index) {
   // Initialisez task form
   // Affichez updates
