ed9fc67ea6e4a4888f17e2998c8ea02e31b4481b
Yassinesahyane89 <yassine.sahyane89@gmail.com>
2022-10-21 09:08:01 +0100

    sprint2

--- starter/assets/js/app.js
+++ starter/assets/js/app.js
@@ -13,14 +13,10 @@ let iconDone = "fa-regular fa-circle-check fa-2xl";
 let NbrTodo = document.getElementById("to-do-tasks-count");
 let NbrInprogress = document.getElementById("in-progress-tasks-count");
 let NbrDone = document.getElementById("done-tasks-count");
-let count = 1;
-let NomberDone = 1;
-let NomberTodo = 1;
-let NomberInprogress = 1;
 
-function button(count, icon, i) {
+function taskHtml(count, icon, i) {
   let btn = `
-        <button class="d-flex align-items-center border-0 border-top">
+        <button class="d-flex align-items-center border-0 border-top" data-bs-toggle="modal" data-bs-target="#update" onclick="editTask(${i})">
             <div class="col-1 text-success">
                 <i class="${icon}"></i> 
             </div>
@@ -40,42 +36,45 @@ function button(count, icon, i) {
   return btn;
 }
 
-function add(i) {
-  if (tasks[i].status == "To Do") {
-    NbrTodo.innerHTML = NomberTodo;
-    ToDO.innerHTML += button(count, iconTOdo, i);
-    count++;
-    NomberTodo++;
-  }
-  if (tasks[i].status == "Done") {
-    NbrDone.innerHTML = NomberDone;
-    Done.innerHTML += button(count, iconDone, i);
-    count++;
-    NomberDone++;
-  }
-  if (tasks[i].status == "In Progress") {
-    NbrInprogress.innerHTML = NomberInprogress;
-    InProgress.innerHTML += button(count, iconInprogress, i);
-    count++;
-    NomberInprogress++;
-  }
-}
-
-function createTask() {
+function reloadTask() {
+  let count = 1;
+  let NomberDone = 1;
+  let NomberTodo = 1;
+  let NomberInprogress = 1;
   // initialiser task form
   for (let i = 0; i < tasks.length; i++) {
-    add(i);
+    if (tasks[i].status == "To Do") {
+      NbrTodo.innerHTML = NomberTodo;
+      ToDO.innerHTML += taskHtml(count, iconTOdo, i);
+      count++;
+      NomberTodo++;
+    }
+    if (tasks[i].status == "Done") {
+      NbrDone.innerHTML = NomberDone;
+      Done.innerHTML += taskHtml(count, iconDone, i);
+      count++;
+      NomberDone++;
+    }
+    if (tasks[i].status == "In Progress") {
+      NbrInprogress.innerHTML = NomberInprogress;
+      InProgress.innerHTML += taskHtml(count, iconInprogress, i);
+      count++;
+      NomberInprogress++;
+    }
   }
   // Afficher le boutton save
 
   // Ouvrir modal form
 }
+reloadTask();
 
-createTask();
-function saveTask() {
-  document.getElementById("BtnSave").addEventListener("click", displayDate);
+function saveTaskEventSetUp() {
+  document.getElementById("BtnSave").addEventListener("click", saveTask);
 
-  function displayDate() {
+  function saveTask() {
+    reloadTasks();
+    // Créez task object
+    // Recuperer task attributes a partir les champs input
     let Newdata = {
       title: document.getElementById("recipient-name").value,
       type: document.querySelector('input[name="flexRadioDefault"]:checked')
@@ -85,26 +84,64 @@ function saveTask() {
       date: document.getElementById("date-input").value,
       description: document.getElementById("message-text").value,
     };
+    // Ajoutez object au Array
     tasks.push(Newdata);
-    add(tasks.length - 1);
+
+    document.getElementById("recipient-name").value = "";
+    document.querySelector('input[name="flexRadioDefault"]:checked').value = "";
+    document.getElementById("Proiority").value = "";
+    document.getElementById("status").value = "";
+    document.getElementById("date-input").value = "";
+    document.getElementById("message-text").value = "";
+
+    // refresh tasks
+    reloadTask();
   }
-  // Recuperer task attributes a partir les champs input
-  // Créez task object
-  // Ajoutez object au Array
-  // refresh tasks
 }
-saveTask();
+saveTaskEventSetUp();
 function editTask(index) {
   // Initialisez task form
+  document.getElementById("Updat-recipient-name").value = tasks[index].title;
+  document.querySelector('input[name="flexRadioDefault"]:checked').value =
+    tasks[index].type;
+  document.getElementById("UpdatProiority").value = tasks[index].priority;
+  document.getElementById("Updatstatus").value = tasks[index].status;
+  document.getElementById("Updat-date-input").value = tasks[index].date;
+  document.getElementById("Updat-message-text").value =
+    tasks[index].description;
   // Affichez updates
+  document
+    .getElementById("UpdatBtnSave")
+    .addEventListener("click", updateTasks);
+  function updateTasks() {
+    reloadTasks();
+    tasks[index].title = document.getElementById("Updat-recipient-name").value;
+    tasks[index].type = document.querySelector(
+      'input[name="flexRadioDefault"]:checked'
+    ).value;
+    tasks[index].priority = document.getElementById("UpdatProiority").value;
+    tasks[index].status = document.getElementById("Updatstatus").value;
+    tasks[index].date = document.getElementById("Updat-date-input").value;
+    tasks[index].description =
+      document.getElementById("Updat-message-text").value;
+    reloadTask();
+  }
   // Delete Button
+  document
+    .getElementById("UpdatBtnDelete")
+    .addEventListener("click", deleteTasks);
+  function deleteTasks() {
+    reloadTasks();
+    tasks.splice(index, 1);
+    reloadTask();
+  }
   // Définir l’index en entrée cachée pour l’utiliser en Update et Delete
   // Definir FORM INPUTS
   // Ouvrir Modal form
 }
-
 function updateTask() {
   // GET TASK ATTRIBUTES FROM INPUTS
+  reloadTasks();
   // Créez task object
   // Remplacer ancienne task par nouvelle task
   // Fermer Modal form
@@ -125,5 +162,8 @@ function initTaskForm() {
 
 function reloadTasks() {
   // Remove tasks elements
+  ToDO.innerHTML = "";
+  Done.innerHTML = "";
+  InProgress.innerHTML = "";
   // Set Task count
 }
